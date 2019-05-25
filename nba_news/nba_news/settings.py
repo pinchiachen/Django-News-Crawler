fabbc37fac578d30fb409a834404002ffce546be
pinchiachen <chenargar@gmail.com>
2019-05-25 15:04:20 +0800

    Add code

--- nba_news/nba_news/settings.py
+++ nba_news/nba_news/settings.py
@@ -138,3 +138,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 9
 }
+
+import django_heroku
+django_heroku.settings(locals())
