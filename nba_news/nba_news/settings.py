f6a5c8dfb58667eec685006d100a15eb66709715
pinchiachen <chenargar@gmail.com>
2019-05-25 16:41:06 +0800

    Add code

--- nba_news/nba_news/settings.py
+++ nba_news/nba_news/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import psycopg2
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,8 +79,8 @@ WSGI_APPLICATION = 'nba_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 '''
 DATABASES = {
     'default': {
