15c9c89a2c76a99749f96af196f91503418dc74c
PinChia Chen <chenargar@gmail.com>
2019-06-04 16:31:58 +0800

    Add files via upload

--- nba_news/crawler/crawler.py
+++ nba_news/crawler/crawler.py
@@ -24,7 +24,7 @@ def news_crawler(pages: int = 10):
     url_base = 'https://nba.udn.com'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
-    # 抓取詳細新聞頁面的URL
+    # 從焦點新聞首頁抓取詳細新聞頁面的URL，預設抓取10頁
     page_detail_url = []
     for i in range(pages):
         res = requests.get(url = 'https://nba.udn.com/nba/cate/6754/-1/newest/%d'%(i+1), headers = headers)
@@ -38,13 +38,13 @@ def news_crawler(pages: int = 10):
     for page_url in page_detail_url:
 
         # 新聞ID
-
         post_id = page_url.split('/')[-1]
 
         # 新聞來源URL
         post_source_url = page_url
 
         try:
+            # 如果抓取的新聞ID已存在，則跳出此層迴圈(為了避免抓取重複新聞)
             if Post.objects.filter(id=post_id).exists():
                 continue
 
@@ -67,6 +67,7 @@ def news_crawler(pages: int = 10):
             for ele in content[1::]:
                 post_content += ele.text
 
+            # 依表格規格建立相關資料
             post_list.append(Post(
                 id = post_id,
                 title = post_title,
@@ -82,9 +83,12 @@ def news_crawler(pages: int = 10):
             print(e)
             continue
 
+    print('-----Crawler End-----')
+
+    # 將大量資料存入資料庫表格
     Post.objects.bulk_create(post_list)
 
-    print('-----Crawler End-----')
+    print('-----Data Saved Complete-----')
 
 
 if __name__ == "__main__":
